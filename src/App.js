f0e3d46330e029124dad7687f315205c6de575c1
apatil241995 <patilanish39@gmail.com>
2023-04-03 17:51:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SvgComponent from './svgComponent';
+import SvgCircle from './svgCircle';
+
+function App() {
+  return (
+    <>
+      <SvgCircle />
+      <SvgComponent />
+    </>
+  );
+}
+
+export default App;
